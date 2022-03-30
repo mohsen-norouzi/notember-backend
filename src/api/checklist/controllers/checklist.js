1ca66ebd9330632208bf5ab3f4082fe5c808c1d3
Mohsen <mohsen.norouzi@live.com>
2022-03-31 03:04:39 +0430

    feature(note): :sparkles: Update schemas
    Add checklist.
    Add label.
    Update note.

---
+++ src/api/checklist/controllers/checklist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  checklist controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::checklist.checklist');
---
+++ src/api/checklist/routes/checklist.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * checklist router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::checklist.checklist');
